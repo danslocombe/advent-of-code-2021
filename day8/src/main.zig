1f7890c4e2f1b81f2ae819995c323be71c2ef7aa
Dan Slocombe <dan.slocombe@hotmail.com>
2021-12-08 22:59:55 +0000

    Cleanup

--- day8/src/main.zig
+++ day8/src/main.zig
@@ -21,8 +21,10 @@ const WireStates = struct {
     }
 
     pub fn is_unique_value(self: WireStates) bool {
-        const on_count = self.get_count();
-        return on_count == 2 or on_count == 4 or on_count == 3 or on_count == 7;
+        return switch (self.get_count()) {
+            2, 4, 3, 7 => true,
+            else => false
+        };
     }
 
     pub fn union_vals(self : WireStates, other : WireStates) WireStates {
@@ -124,28 +126,22 @@ const SegmentValues = struct {
         var inferred_digits : [10]WireStates = undefined;
 
         for (self.digit_values) |value| {
-            const on_count = value.get_count();
-            if (on_count == 2) {
-                inferred_digits[1] = value;
-            }
-            else if (on_count == 4) {
-                inferred_digits[4] = value;
-            }
-            else if (on_count == 3) {
-                inferred_digits[7] = value;
-            }
-            else if (on_count == 7) {
-                inferred_digits[8] = value;
+            switch (value.get_count()) {
+                2 => inferred_digits[1] = value,
+                3 => inferred_digits[7] = value,
+                4 => inferred_digits[4] = value,
+                7 => inferred_digits[8] = value,
+                else => {},
             }
         }
 
-        // Find with count 6
+        // Find all with 6 segments.
         inferred_digits[9] = find_superset_with_count_except(inferred_digits[4], self.digit_values[0..], null, 6);
         inferred_digits[0] = find_superset_with_count_except(inferred_digits[1], self.digit_values[0..], inferred_digits[9], 6);
         const exclusion_to_find_six = [2]WireStates{inferred_digits[9], inferred_digits[0]};
         inferred_digits[6] = find_with_count_except(self.digit_values[0..], exclusion_to_find_six[0..], 6);
 
-        // Find with count 5
+        // Find all with 5 segments.
         inferred_digits[3] = find_superset_with_count_except(inferred_digits[1], self.digit_values[0..], null, 5);
         inferred_digits[5] = find_superset_with_count_except(inferred_digits[4].intersect(inferred_digits[6]), self.digit_values[0..], inferred_digits[3], 5);
         const exclusion_to_find_two = [2]WireStates{inferred_digits[3], inferred_digits[5]};
@@ -212,7 +208,6 @@ pub fn main() anyerror!void {
     var arena = std.heap.ArenaAllocator.init(gpa.allocator());
     defer arena.deinit();
 
-
     var allocator = arena.allocator();
 
     var lines = try lib.Lines.from_file(allocator, std.fs.cwd(), "day8_input.txt");
@@ -226,6 +221,5 @@ pub fn main() anyerror!void {
     var sub_state = SubDisplayState.init(segment_values.items);
 
     std.log.info("Unique states {d}", .{sub_state.get_unique_value_count()});
-
     std.log.info("Sum {d}", .{sub_state.get_digit_sum()});
 }
